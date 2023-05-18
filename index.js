754680efee76aff7565155e7c4c03654e4e2a484
xDelusion <xdelusion@icloud.com>
2023-05-18 17:39:25 +0300

    :))

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ class Employee {
     //idBadge = (id) => {(this.position, this.name) = id};  (((error)))
 }
 
-//const emp1 = new Employee(employeesJSON);
+//const emp1 = new Employee(employeesJSON); ((to add to params of constructor))
 const emp2 = employeesJSON.map((emp) => ({ position: emp.position, name: emp.name}));
-
+//but line 18 made it possible to log the position and name
   console.log(emp2);
 // console.log(emp1.idBadge);
 
@@ -28,12 +28,15 @@ class Manager extends Employee {
     constructor(bonusPercentage) {
         this.bonusPercentage = bonusPercentage;
     
-        super("bonusPercentage")
+        super("bonusPercentage");
     }
 
     salaryIncrease() {
-
+        return (this.salary + 1);
     }
 
 
 }
+
+const manager1 = new Manager(5);
+console.log(manager1.salaryIncrease(increaseAmount));
